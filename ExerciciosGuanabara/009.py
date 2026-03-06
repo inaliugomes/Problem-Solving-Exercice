4dcdb0c31625a65d7b9e5b166cf434d287ef4c32
inaliugomes <inaliugomes@gmail.com>
2026-03-06 17:30:23 +0100

    exercicio 008 a 010

---
+++ ExerciciosGuanabara/009.py
@@ -0,0 +1,6 @@
+tabuada = int(input("Informe um numero para ver sua tabuada: "))
+
+print("--------------")
+for c in range(1,11):
+    print(f" {tabuada} x {c} = {tabuada*c}")
+print("--------------")
---
+++ ExerciciosGuanabara/010.py
@@ -0,0 +1,5 @@
+reais = float(input("Informe o valor da Reais: "))
+
+dolar = reais / 3.27
+
+print(f"Com {reais} voce comprar {dolar:.2f} dollars")
