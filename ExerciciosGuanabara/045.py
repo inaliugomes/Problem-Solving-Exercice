dc6e1253d78d1a4e145a6e255c1a2d618c525b56
inaliugomes <inaliugomes@gmail.com>
2026-03-18 13:06:37 +0100

    Ultimo exercicio estrutura de repeticao

--- ExerciciosGuanabara/045.py
+++ ExerciciosGuanabara/045.py
@@ -9,4 +9,28 @@ opcao = int(input("Sua opcao: "))
 
 opcao_computador = randint(0,2)
 
-print(opcao_computador)
+
+if opcao == 0:
+    if opcao_computador == 0:
+        print("Voce jogou Pedra o computador jogou pedra EMPATE!")
+    elif opcao_computador == 1:
+        print("Voce jogou Pedra o computador jogou Papel COMPUTADOR GANHOU!")
+    else:
+        print("Voce jogou Pedra o computador jogou TESOURA VOCE GANHOU!")
+elif opcao == 1:
+    if opcao_computador == 0:
+        print("Voce jogou PAPEL o computador jogou PEDRA VOCE GANHOU!")
+    elif opcao_computador == 1:
+        print("Voce jogou PAPEL o computador jogou Papel  EMPATE!")
+    else:
+        print("Voce jogou PAPEL o computador jogou TESOURA COMPUTADOR GANHOU!")
+elif opcao == 2:
+    if opcao_computador == 0:
+        print("Voce jogou TESOURA o computador jogou PEDRA COMPUTADOR GANHOU!")
+    elif opcao_computador == 1:
+        print("Voce jogou TESOURA o computador jogou Papel  VOCE GANHOU!")
+    else:
+        print("Voce jogou TESOURA o computador jogou TESOURA  EMPATE!")
+
+
+
