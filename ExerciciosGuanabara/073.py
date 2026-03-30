a2fdc8d1854be68ab6179438e7ea9288bfd2bce4
inaliugomes <inaliugomes@gmail.com>
2026-03-30 23:43:19 +0200

    073

---
+++ ExerciciosGuanabara/073.py
@@ -0,0 +1,27 @@
+times_brasileirao_2017 = [
+    "Palmeiras",
+    "Flamengo",
+    "Atlético Mineiro",
+    "Santos",
+    "Botafogo",
+    "Athletico Paranaense",
+    "Corinthians",
+    "Grêmio",
+    "Fluminense",
+    "Chapecoense",
+    "São Paulo",
+    "Cruzeiro",
+    "Vasco da Gama",
+    "Sport Recife",
+    "Bahia",
+    "Vitória",
+    "Coritiba",
+    "Avaí",
+    "Ponte Preta",
+    "Atlético Goianiense"
+]
+
+print(f"Os primeiros 5 equipos : {times_brasileirao_2017[:5]}")
+print(f"Os ultimos 4 equipos : {times_brasileirao_2017[-4:]}")
+print(sorted(times_brasileirao_2017))
+print(f"O chapecoense entá na {times_brasileirao_2017.index("Chapecoense")} posicao")
