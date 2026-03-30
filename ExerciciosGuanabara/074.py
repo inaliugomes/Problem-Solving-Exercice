935ea2c97232c12881c945d523b9b63cde57ba5d
inaliugomes <inaliugomes@gmail.com>
2026-03-30 23:51:27 +0200

    Cambio 075

--- ExerciciosGuanabara/074.py
+++ ExerciciosGuanabara/074.py
@@ -2,7 +2,11 @@ from random import randint
 
 valor_sorteado = (randint(0, 10),randint(0, 10),randint(0, 10),randint(0, 10),randint(0, 10))
 valores_ordenados = sorted(valor_sorteado)
-print(f"Os valores sorteados foram: {valores_ordenados}")
 
-print(f"O maior valor sorteado foi: {valores_ordenados[-1]}")
+print(f"Os valores sorteados foram:",end=" ")
+for valor in valores_ordenados:
+    print(valor,end=" ")
+
+
+print(f"\nO maior valor sorteado foi: {valores_ordenados[-1]}")
 print(f"O menor valor sorteado foi: {valores_ordenados[0]}")
