f1af9c9c692a8a0ceac1912dcb01aba58848a469
inaliugomes <inaliugomes@gmail.com>
2026-04-06 21:52:58 +0200

    088

---
+++ ExerciciosGuanabara/088.py
@@ -0,0 +1,27 @@
+from random import randint
+from time import sleep
+print("-="*20)
+print(f"{' '*10}JOGA NA MEGA SENA")
+print("-="*20)
+
+sorteio = int(input("Quantos jogos deseja sortear? "))
+
+
+for c in range(0, sorteio):
+    numeros_sorteados = []
+    for x in range(0, 6):
+        numero_aleatorio = randint(1, 60)
+        if numero_aleatorio not in numeros_sorteados:
+            numeros_sorteados.append(numero_aleatorio)
+        else:
+            while True:
+                numero_aleatorio = randint(1, 60)
+                if numero_aleatorio not in numeros_sorteados:
+                    numeros_sorteados.append(numero_aleatorio)
+                    break
+
+    numeros_sorteados.sort()
+    print(numeros_sorteados)
+    sleep(1)
+
+
