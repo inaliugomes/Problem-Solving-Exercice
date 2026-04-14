4d187fec0d802aed12a45ad752e84e2679f7d75a
inaliugomes <inaliugomes@gmail.com>
2026-04-14 23:32:59 +0200

    094 finalizado

--- ExerciciosGuanabara/094.py
+++ ExerciciosGuanabara/094.py
@@ -2,6 +2,7 @@ from operator import truediv
 
 lista_pessoas = list()
 media_idade = 0
+mulhes = []
 continuar = True
 while continuar:
     dados_pessoal = {}
@@ -15,6 +16,8 @@ while continuar:
     dados_pessoal['idade'] = int(input("Idade: "))
     media_idade += dados_pessoal['idade']
     lista_pessoas.append(dados_pessoal)
+    if dados_pessoal['sexo'] in 'Ff':
+        mulhes.append(dados_pessoal['nome'])
     while True:
         parar = str(input("Deseja continuar? [S/N]"))
         if parar =='N' or parar == 'n':
@@ -29,3 +32,8 @@ media = media_idade / len(lista_pessoas)
 print("-="*20)
 print(f"A) Ao todo temos {len(lista_pessoas)} pessoas cadastradas.")
 print(f"B) A média de idade é de {media}")
+print(f"C) As mulheres cadastradas foram {mulhes}")
+print(f"D) Lista das pessoas que estao acima da media: ")
+for pessoa in lista_pessoas:
+    if pessoa['idade'] > media:
+        print(f"    nome={pessoa['nome']}; sexo={pessoa['sexo']} ; idade={pessoa['idade']}")
