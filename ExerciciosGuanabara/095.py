8d1bacc9bb098e2fbabc67336a185b27fd476894
inaliugomes <inaliugomes@gmail.com>
2026-04-20 18:21:44 +0200

    finalizado el exercicio 092

--- ExerciciosGuanabara/095.py
+++ ExerciciosGuanabara/095.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 lista_jogadores = []
 
 continuar = 'S'
@@ -16,5 +18,25 @@ while continuar == 'S':
     if continuar == 'N':
         break
 
-print(lista_jogadores)
 
+print("-="*20)
+print(f"{'Cod':<6} {'Nome':<15} {'Gols':<20}{'Total':<6}")
+print("-"*47)
+for c in range(0,len(lista_jogadores)):
+    total_golo = sum(lista_jogadores[c]['golos'])
+    gols_str = str(lista_jogadores[c]['golos'])
+    print(f"{c:<6} {lista_jogadores[c]['nome']:<15} {gols_str:<20} {total_golo:<6}")
+
+
+while True:
+
+    jogador = int(input(" Mostrar dados de qual jogador ? (999 para parar): )"))
+    if jogador == 999:
+        break
+    if jogador <= len(lista_jogadores):
+        print(f"LEVANTAMENTO DO JOGADOR {lista_jogadores[jogador]['nome']}: ")
+        for c,p in enumerate(lista_jogadores[jogador]['golos']):
+            print(f"No jogo {c+1} fez {p} gols")
+
+    else:
+        print(f"ERRO! Nao existe jogador com codigo {jogador}")
