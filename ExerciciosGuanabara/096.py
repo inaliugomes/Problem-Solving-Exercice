5f59610826d3ba6d2713e6080f30af9624283560
inaliugomes <inaliugomes@gmail.com>
2026-04-20 19:25:05 +0200

    09&

---
+++ ExerciciosGuanabara/096.py
@@ -0,0 +1,10 @@
+def area(largura,comprimento):
+    print(f" A area de um terreno {largura} x {comprimento} é {largura*comprimento}")
+
+largura = float(input("Largura: "))
+comprimento = float(input("Comprimento: "))
+
+
+area(largura,comprimento)
+
+
