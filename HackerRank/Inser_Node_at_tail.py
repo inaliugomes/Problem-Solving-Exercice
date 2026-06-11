8851569a8f84f2b13f80a9c9384f0e5456c1a473
INlaiu <inaliugomes@gmail.com>
2026-06-11 17:11:41 +0200

    Inser_Node_at_tail hackerRank exercice

---
+++ HackerRank/Inser_Node_at_tail.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    new_node = SinglyLinkedListNode(data)
+    current = head
+    if current is None:
+        head = new_node
+    else:
+        while current.next:
+            current = current.next
+        current.next = new_node
+    return head
