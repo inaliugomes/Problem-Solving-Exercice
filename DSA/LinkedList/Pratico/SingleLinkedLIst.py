920f555fd5038703c92b0a8acfc6206508bf401b
INlaiu <inaliugomes@gmail.com>
2026-06-18 23:18:32 +0200

    revi single linked list

--- DSA/LinkedList/Pratico/SingleLinkedLIst.py
+++ DSA/LinkedList/Pratico/SingleLinkedLIst.py
@@ -1,106 +1,88 @@
-from bcc.tcp import flags2str
-from pygments.lexer import words
-
-
-class Node:
-
-    #Method de inicializacion del Node
-    def __init__(self,data = None):
+class Node(object):
+    def __init__(self,data=None):
         self.data = data
         self.next = None
 
 
-
-class SingleLinkedList:
-
+class SinglyLinkedList:
     def __init__(self):
         self.tail = None
         self.head = None
         self.count = 0
 
     def append(self,data):
-        node = Node(data)
-        if self.head:
+        node  = Node(data)
+
+        if self.head :
             self.head.next = node
             self.head = node
-
         else:
             self.tail = node
-            self.head = node
-
-        self.count +=1
+            self.head =node
+        self.count += 1
 
     def iter(self):
         current = self.tail
-
-        while  current:
-            val =current.data
+        while current:
+            value = current.data
             current = current.next
-            yield val
+            yield value
 
     def delete(self,data):
-
         current = self.tail
         prev = self.tail
 
         while current:
-            if current.data ==  data:
+            if current.data == data:
                 if current == self.tail:
                     self.tail = current.next
                 else:
                     prev.next = current.next
-                self.count -=1
+                self.count -= 1
                 return
             prev = current
             current = current.next
 
     def search(self,data):
-
         for node in self.iter():
             if data == node:
                 return True
-            return False
+        return False
 
     def __getitem__(self, index):
-        if index > self.count-1:
+        if index > self.count -1:
             raise Exception("Index out of range")
         current = self.tail
-
         for n in range(index):
             current = current.next
         return current.data
 
-    def __setitem__(self,index,data):
-
-        if index > self.count-1:
+    def __setitem__(self,index,value):
+        if index > self.count -1:
             raise Exception("Index out of range")
         current = self.tail
         for n in range(index):
             current = current.next
-        current.data = data
+        current.data = value
 
 
-
-words = SingleLinkedList()
-
+words = SinglyLinkedList()
 words.append('foo')
 words.append('bar')
 words.append('bim')
 words.append('baz')
 words.append('quux')
 
-print("acess by index")
-print(f"here is a node: {words[0]}")
+print("access by index")
+print("here is a node: {}".format(words[1]))
 
 print("modify by index")
 words[4] = "Quux"
 print("Modified node by index: {}".format(words[4]))
 
-
 print("This list has {} elements.".format(words.count))
 for word in words.iter():
     print("Got this data: {}".format(word))
-
 
 if words.search('foo'):
     print("Found foo in the list.")
@@ -112,7 +94,6 @@ words.delete('bim')
 print("List now has {} elements".format(words.count))
 for word in words.iter():
     print("data: {}".format(word))
-
 
 print("delete the first item in the list")
 words.delete('foo')
